95ee70c701cf9892fa5cca89e6b4031d8076887e
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-11-07 14:45:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.docx"
+resume_file = current_dir / "assets" / "cv.docx"
 profile_pic = current_dir / "assets" / "picture.png"
 
 
